26095f671f291168fcf7f3d907f780c914d4e97d
ziraj <mohamedsirajudeen@notionpress.com>
2019-02-11 06:47:40 +0530

    ComponentCreation-A002-7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MovielistComponent } from './movielist/movielist.component';
+import { MoviedetailsComponent } from './moviedetails/moviedetails.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MovielistComponent,
+    MoviedetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/movielist/movielist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movielist',
+  templateUrl: './movielist.component.html',
+  styleUrls: ['./movielist.component.css']
+})
+export class MovielistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
