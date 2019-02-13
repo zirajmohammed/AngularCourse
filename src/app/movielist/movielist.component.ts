d363ab80a246e9a1f8c03402135f8973b4e5f1bc
ziraj <mohamedsirajudeen@notionpress.com>
2019-02-13 06:33:18 +0530

    @ViewChild-A003-10

--- src/app/movielist/movielist.component.ts
+++ src/app/movielist/movielist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter,ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter,ViewEncapsulation, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-movielist',
@@ -14,6 +14,9 @@ export class MovielistComponent implements OnInit {
   
   @Output() movieClick = new EventEmitter();
   @Output('outAlias') movieClickAlias = new EventEmitter();
+
+  @ViewChild('addMovieEvent') header: ElementRef;
+
   dataBinding: boolean = false;
   twoWayBinding: string = '';
 
@@ -22,10 +25,12 @@ export class MovielistComponent implements OnInit {
   ngOnInit() {
   }
 
-  addMovieName(inputElement:HTMLInputElement){
+  addMovieName(){
+    console.log(this.header);
     this.movieList.push({
       _id: "741236589",
-      movietitle: inputElement.value,
+      movietitle: this.header.nativeElement
+      .value,
       genre: "Fantasy",
       rating: 5,
       desc: "velit in sunt occaecat exercitation qui amet ex elit labore excepteur occaecat veniam nisi irure Lorem minim excepteur irure aliquip"
